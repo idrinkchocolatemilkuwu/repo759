8fe92d5edf24648511a1bb4897bc5408fe2b60c6
Yurim Lee <ylee739@wisc.edu>
2022-09-12 12:22:20 -0500

    Upload New File

---
+++ HW01/task6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  int N;
+  N = atoi(argv[1]);
+	for (int i = 0; i < N+1; i++) {
+		printf("%i ",i);
+	}
+	cout << "\n";
+	for (int i = N; i > -1; i--) {
+		cout << i << " ";
+	}
+	cout << "\n";
+  return 0;
+}
