1b560b6e79b1fbe212ab324adb511a63913388e4
Yurim Lee <ylee739@wisc.edu>
2022-11-11 01:38:55 -0600

    Replace task3.cpp

--- HW09/task3.cpp
+++ HW09/task3.cpp
@@ -1,7 +1,8 @@
-#include "mpi.h"
+#include <mpi.h>
 #include <iostream>
 #include <chrono>
 #include <random>
+#include <stdio.h>
 using namespace std;
 using chrono::duration;
 
@@ -30,42 +31,49 @@ int main(int argc, char** argv){
 	int rank, size;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	//MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Status status;
 
-	duration<double, milli> t0, t1;
+	//duration<double, milli> t0, t1;
+	double t0, t1;
 
 	//implement the procedure listed
 	if (rank == 0){
 		//start timing t0
-		chrono::high_resolution_clock::time_point start;
-		chrono::high_resolution_clock::time_point end;
-		start = chrono::high_resolution_clock::now();
+		double start0, end0;
+		start0 = MPI_Wtime();
 		//rank 0 should send array "A" and receive array "B"
 		MPI_Send(A, n, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
 		MPI_Recv(B, n, MPI_FLOAT, 1, 1, MPI_COMM_WORLD, &status);
-		end = chrono::high_resolution_clock::now();
+		end0 = MPI_Wtime();
 		//measure t0
-		t0 = std::chrono::duration_cast<duration<double, milli>>(end - start);
+		t0 = (end0 - start0) * 1000;
+		//send t0
+		MPI_Send(&t0, 1, MPI_DOUBLE, 1, 2, MPI_COMM_WORLD);
 		//receive t1
-		MPI_Recv(&t1, 1, MPI_DOUBLE, 1,  2, MPI_COMM_WORLD, &status);
-		//print t0 + t1
-		printf("%f\n", t0 + t1);
+		//MPI_Recv(&t1, 1, MPI_DOUBLE, 1, 3, MPI_COMM_WORLD, &status);
 
 	}else if (rank == 1){
 		//start timing t1
-		chrono::high_resolution_clock::time_point start;
-		chrono::high_resolution_clock::time_point end;
-		start = chrono::high_resolution_clock::now();
-		//rank 1 should receive array "A" and receive array "B"
+		double start1, end1;
+		start1 = MPI_Wtime();
+		//rank 1 should receive array "A" and send array "B"
 		MPI_Recv(A, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
 		MPI_Send(B, n, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
-		end = chrono::high_resolution_clock::now();
+		end1 = MPI_Wtime();
 		//measure t1
-		t1 = std::chrono::duration_cast<duration<double, milli>>(end-start);
+		t1 = (end1 - start1) * 1000;
+		//receive t0
+		MPI_Recv(&t0, 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
 		//send t1
-		MPI_Send(&t1, 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+		//MPI_Send(&t1, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+
+		//print t0 + t1
+		printf("%f\n", t0 + t1);
+		//std::cout << t0 + t1 << std::endl;
 	}
+	//print t0 + t1
+	//printf("%f\n", t0 + t1);
 
 	MPI_Finalize();
 }
