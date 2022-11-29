055b14fce3253e5a87809ed0627ef5e18d470b1d
Yurim Lee <ylee739@wisc.edu>
2022-11-29 12:54:39 -0600

    Replace checkpoint_gaussian_filter.cu

--- FinalProject759/code/checkpoint_gaussian_filter.cu
+++ FinalProject759/code/checkpoint_gaussian_filter.cu
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <omp.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -36,12 +37,12 @@ __global__ void color_to_grayscale(uint8_t* img, uint8_t* mono_img, int img_widt
     }
 
     int index;
-    index = y * image_width * 3 + x * 3;
+    index = y * img_width * 3 + x * 3;
     //"luminosity method" weighs red, green, blue according to wavelength
     //instead of taking average
-    mono_img[y * image_width + x] = (299 * img[index] + 587 * img[index + 1] + 114 * img[index + 2]) / 1000;
+    mono_img[y * img_width + x] = (299 * img[index] + 587 * img[index + 1] + 114 * img[index + 2]) / 1000;
 
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
 
 }
 
@@ -63,7 +64,7 @@ __host__ void Gaussian_filter(float sigma, float** computed_filter, float* compu
     cudaMallocManaged((void**)&filter, filter_size * sizeof(float));
     
     //compute Gaussian filter
-    sum = 0;
+    float sum = 0;
     for (int i = 0; i < filter_size; i++){
         filter[i] = exp(-pow((i - filter_center) / sigma, 2) / 2) / (sigma * sqrt(2 * M_PI));
         sum += filter[i];
@@ -83,31 +84,43 @@ __host__ void Gaussian_filter(float sigma, float** computed_filter, float* compu
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-__global__ void convolve(uint8_t* img, uint8_t* img_output, int img_width, int img_height, float* filter, int filter_size)
+__host__ uint8_t boundary_condition(uint8_t* img, int img_width, int img_height, float* filter, int filter_size, int i, int j)
+{
+    //boundary condition for convolution
+    //try boundary condition from HW02
+
+    if ((0 <= i && i < img_height) && (0 <= j && j < img_width)){
+        return img[i * img_width + j];
+    }
+    else if ((0 <= i && i < img_height) || (0 <= j && j < img_width)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+__host__ void convolve(uint8_t* img, uint8_t* img_output, int img_width, int img_height, float* filter, int filter_size)
 {
     //input 1D arrays of grayscale image "img" and Gaussian filter "filter"
     //where filter is filter_size x filter_size
     //convolve the image with Gaussian filter
     //store the convolved output at "img_output"
 
-    int x, y;
-    x = blockDim.x * blockIdx.x + threadIdx.x;
-    y = blockDim.y * blockIdx.y + threadIdx.y;
-
-    if (y >= img_height || x >= img_width){
-        return;
+    //set img_output to 0
+    for (int i = 0; i < img_width * img_height; i++){
+        img_output[i] = 0;
     }
 
     //do convolution
-    //output[x,y] = sum over i(sum over j(image[i,j]*filter[x+i-(m-1)/2,y+j-(m-1)/2]))
-    //where m is filter size 
-    //note i ranges from minimum(0, x+i-(m-1)/2)
-    //as i > 0 and x+i-(m-1)/2 > 0, i > (m-1)/2 -x
-    //and i < image width and x+i-(m-1)/2 < m, i < 3m/2
-    float sum = 0;
-    for (int i = max(0, y + 1 - filter_size / 2); i < min(img_height, y + 1 + filter_size / 2); i++){
-        for (int j = max(0, x + 1 - filter_size / 2); j < min(img_width, x + 1+ filter_size / 2); j++){
-            sum += img[i * img_width + j] * filter[(y + i -)]
+    #pragma omp parallel for collapse(2)
+    for (int x = 0; x < img_height; x++){
+        for (int y = 0; y < img_width; y++){
+            for (int i = 0; i < filter_size; i++){
+                for (int j = 0; j < filter_size; j++){
+                    img_output[x * img_width + y] += filter[i * filter_size + j] * boundary_condition(img, img_width, img_height, filter, filter_size, x + i - (filter_size - 1) / 2, y + j - (filter_size - 1) / 2);
+                }
+            }
         }
     }
 }
@@ -135,7 +148,7 @@ int main(){
 
     //change image to grayscale
     uint8_t* grayscale_image;
-    cudaMallocManaged((void**)&grayscale, image_width * image_height * sizeof(uint8_t));
+    cudaMallocManaged((void**)&grayscale_image, image_width * image_height * sizeof(uint8_t));
     color_to_grayscale<<<dim3(1024,1024), dim3(2,2)>>>(device_image, grayscale_image, image_width, image_height);
     
     //compute Gaussian filter
@@ -145,9 +158,12 @@ int main(){
     Gaussian_filter(gaussian_filter_sigma, &gaussian_filter, &gaussian_filter_size);
     
     //convolve the image with Gaussian filter 
-
+    uint8_t* convolved_image;
+    cudaMallocManaged((void**)&convolved_image, image_width * image_height * sizeof(uint8_t));
+    convolve(grayscale_image, convolved_image, image_width, image_height, gaussian_filter, gaussian_filter_size);
 
     //save the loaded image
     const char* output_image_path = "/data/data_ustv/home/ylee739/edge-detection-filter/output.jpg";
-    stbi_write_jpg(output_image_path, image_width, image_height, 3, image, 100);
+    stbi_write_jpg(output_image_path, image_width, image_height, 1, convolved_image, 100);
+    //stbi_write_jpg(output_image_path, image_width, image_height, 3, image, 100);
 }
